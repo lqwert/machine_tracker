56f87b5f453ca1e774b771ef2d9d352f0a34b4e5
Luca Meyer <kiste@Lucas-MacBook-Pro.local>
2020-07-03 02:10:35 +0200

    Added storage, settings, scanner

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from 'src/app/shared/_settings/settings.service';
+import { ActivatedRoute } from '@angular/router';
+import { MachineService, MachineModel } from 'src/app/shared';
 
 @Component({
   selector: 'app-settings',
@@ -8,18 +10,33 @@ import { SettingsService } from 'src/app/shared/_settings/settings.service';
 })
 export class SettingsPage implements OnInit {
 
+  defaultMachine: string;
   dummyData: boolean;
   disableScanner: boolean
   timerSpeed: boolean;
 
+  machines: MachineModel[];
+
   constructor(
-    private settingsService: SettingsService
+    private settingsService: SettingsService,
+    private machineService: MachineService,
+    public activeRoute: ActivatedRoute,
   ) { }
 
   ngOnInit() {
+    this.activeRoute.params.subscribe(_ => {
+      this.machineService.getMachines().subscribe(res => {
+        this.machines = res;
+      })
+    })
+    this.defaultMachine = this.settingsService.getDefaultMachineValue();
     this.dummyData = this.settingsService.getDummyDataValue();
     this.disableScanner = this.settingsService.getDisableScannerValue();
     this.timerSpeed = this.settingsService.getTimerSpeedValue();
+  }
+
+  changeDefaultMachine(change) {
+    this.settingsService.changeDefaultMachine(change.detail.value);
   }
 
   changeDummyData(change: CustomEvent) {
